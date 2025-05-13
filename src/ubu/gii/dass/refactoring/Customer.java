c488c3f2e200c059de362beb04738a9bab53e200
Tibbitea <62241263+Tibbitea@users.noreply.github.com>
2025-05-13 16:55:42 +0200

    #1 Extract method a Rental
    <?xml version="1.0" encoding="UTF-8"?>
    <session version="1.0">
    <refactoring comment="Extract method &apos;private int rental(int frequentRenterPoints, Rental each)&apos; from &apos;ubu.gii.dass.refactoring.Customer.generateTextStatement()&apos; to &apos;ubu.gii.dass.refactoring.Customer&apos;&#x0D;&#x0A;- Original project: &apos;refactoring-fowler-example&apos;&#x0D;&#x0A;- Method name: &apos;rental&apos;&#x0D;&#x0A;- Destination type: &apos;ubu.gii.dass.refactoring.Customer&apos;&#x0D;&#x0A;- Declared visibility: &apos;private&apos;" comments="false" description="Extract method &apos;rental&apos;" destination="0" exceptions="false" flags="786434" id="org.eclipse.jdt.ui.extract.method" input="/src&lt;ubu.gii.dass.refactoring{Customer.java" name="rental" project="refactoring-fowler-example" replace="false" selection="1131 121" version="1.0" visibility="2"/>
    </session>

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -44,9 +44,7 @@ public class Customer {
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints = rental(frequentRenterPoints, each);
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
@@ -57,6 +55,13 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+
+	private int rental(int frequentRenterPoints, Rental each) {
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& each.getDaysRented() > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
 	}
 
 	private double cantidadDeLineas(double thisAmount, Rental each) {
