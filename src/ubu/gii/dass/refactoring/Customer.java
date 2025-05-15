2a9c0e69a63e9d019d0377a8b927e922a12acf27
César Rodríguez Vilagrá <128821412+CesarRodrigu@users.noreply.github.com>
2025-05-15 18:20:59 +0200

    #3 Move getFrecuentRenterPoints
    <?xml version="1.0" encoding="UTF-8"?>
    <session version="1.0">
    <refactoring comment="Move method &apos;ubu.gii.dass.refactoring.Customer.getFrecuentRenterPoints(...)&apos; to &apos;ubu.gii.dass.refactoring.Customer.getFrecuentRenterPoints(...).each&apos;&#x0D;&#x0A;- Original project: &apos;refactoring-fowler-example&apos;&#x0D;&#x0A;- Original element: &apos;ubu.gii.dass.refactoring.Customer.getFrecuentRenterPoints(...)&apos;&#x0D;&#x0A;- Moved method: N/A&#x0D;&#x0A;- Target element: &apos;ubu.gii.dass.refactoring.Customer.getFrecuentRenterPoints(...).each&apos;&#x0D;&#x0A;- Method name: &apos;getFrecuentRenterPoints&apos;" deprecate="true" description="Move method &apos;getFrecuentRenterPoints&apos;" flags="786438" getter="true" id="org.eclipse.jdt.ui.move.method" inline="true" input="/src&lt;ubu.gii.dass.refactoring{Customer.java[Customer~getFrecuentRenterPoints~QRental;" name="getFrecuentRenterPoints" project="refactoring-fowler-example" remove="true" setter="true" targetIndex="0" targetName="customer" version="1.0"/>
    </session>

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -44,7 +44,7 @@ public class Customer {
 			thisAmount = each.getCharge();
 
 			// add frequent renter points
-			frequentRenterPoints += getFrecuentRenterPoints(each);
+			frequentRenterPoints += each.getFrecuentRenterPoints();
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
@@ -53,12 +53,5 @@ public class Customer {
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
-	}
-
-	private int getFrecuentRenterPoints(Rental each) {
-		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-			return 2;
-		return 1;
 	}
 }
